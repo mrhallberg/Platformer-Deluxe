16f571ce74d3ca7e56efe23bbe39bb2bc1767125
mrhallberg <mr.hallberg@hotmail.com>
2016-02-08 11:35:33 +0100

    Animation and basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	public bool isJumping = false;
+
+	Animator animator;
+	Rigidbody2D rigidbody;
+
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			if (!isJumping) {
+				isJumping = true;
+				rigidbody.AddForce (new Vector2 (0, jumpForce));
+			}
+		}
+		if (Input.GetAxisRaw("Horizontal") != 0) {
+			animator.SetBool ("IsWalking", true);
+			transform.position += new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+		} else {
+			animator.SetBool ("IsWalking", false);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		isJumping = false;
+	}
+}
